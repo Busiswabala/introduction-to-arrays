20934795612b8b5b6c16a3216f007f6b77ba664d
Busiswabala <balabusiswa22@gmail.com>
2026-05-22 08:41:58 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,20 @@
+// TODO: Create an empty array called colors
+let colors = [];
+// TODO: Add three colors to the array using push()
+const firstColor = colors.push("red");
+const secondColor = colors.push("purple");
+const thirdColor = colors.push("blue");
+// TODO: Create another array called numbers with five numbers
+let numbers = [11, 12, 13, 14, 15];
+// TODO: Add a new color to the beginning of the colors array
+let newColor = colors.unshift("green");
+// TODO: Print the length of both arrays
+console.log(colors.length); 
+console.log(numbers.length);
+// additional print outs
+console.log(firstColor); 
+console.log(secondColor); 
+console.log(thirdColor);
+console.log(newColor);
+console.log(colors);
+console.log(numbers);
